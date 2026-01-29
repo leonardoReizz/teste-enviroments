7c6c72d6cee42d7b2eb4a861d8b84335f64c91b8
Leonardo Reis <amarantee.reis@gmail.com>
2026-01-29 14:55:12 -0300

    test

--- vite.config.js
+++ vite.config.js
@@ -11,10 +11,5 @@ export default defineConfig({
     port: 4000,
     host: "0.0.0.0",
     allowedHosts: ["cmky8bfih000052f54vu-tnnb02.sysko.io", "app.sysko.io"],
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  }
 })
